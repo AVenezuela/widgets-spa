bf3207f3a6f2d61d71ed0b024cf5b3d866b29890
AVenezuela <andre.venezuela@live.com>
2017-10-17 08:36:37 -0200

    Readme file

--- sql-files/user-table.sql
+++ sql-files/user-table.sql
@@ -1,7 +1,7 @@
-USE [Widgets]
+USE [Widget]
 GO
 
-/****** Object:  Table [dbo].[User]    Script Date: 16/10/2017 10:54:38 ******/
+/****** Object:  Table [dbo].[User]    Script Date: 17/10/2017 08:36:03 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,8 +11,8 @@ GO
 CREATE TABLE [dbo].[User](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [varchar](150) NOT NULL,
-	[Gravatar] [varchar](400) NULL,
-PRIMARY KEY CLUSTERED 
+	[Gravatar] [varchar](250) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
--- sql-files/widget-table.sql
+++ sql-files/widget-table.sql
@@ -1,7 +1,7 @@
-USE [Widgets]
+USE [Widget]
 GO
 
-/****** Object:  Table [dbo].[Widget]    Script Date: 16/10/2017 10:54:59 ******/
+/****** Object:  Table [dbo].[Widget]    Script Date: 17/10/2017 08:36:24 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,12 +10,12 @@ GO
 
 CREATE TABLE [dbo].[Widget](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](150) NOT NULL,
 	[Color] [varchar](50) NOT NULL,
 	[Price] [decimal](18, 2) NOT NULL,
 	[Inventory] [int] NOT NULL,
 	[Melts] [bit] NOT NULL,
- CONSTRAINT [PK__Widget__3214EC071228009B] PRIMARY KEY CLUSTERED 
+	[Name] [varchar](150) NOT NULL,
+ CONSTRAINT [PK_Widgets] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
